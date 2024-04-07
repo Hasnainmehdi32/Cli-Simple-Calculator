73692bd349a3447f531f7ee5c9869d44b3db8aea
Hasnain312 <165533685+Saqlain746@users.noreply.github.com>
2024-04-07 11:02:38 +0500

    push my Cli-Simple-Calculator to github

---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter firstnumber", type: "number", name: "firstnumber" },
+    { message: "Enter secondnumber", type: "number", name: "secondnumber" },
+    {
+        message: "Select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
